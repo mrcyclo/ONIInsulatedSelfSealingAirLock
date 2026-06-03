dff9efde15e5a7ba31b1dacf967e01939f0e6dcc
Triggernometry <detectivespacecore@gmail.com>
2026-06-03 01:35:19 -0500

    Reformat set door states; change to Postfix; add OnSpawn postfix to fix leak on loading issue

--- Door_SetSimState.cs
+++ Door_SetSimState.cs
@@ -2,32 +2,70 @@
 using System;
 using System.Collections.Generic;
 using System.Reflection;
+using static STRINGS.BUILDINGS.PREFABS.DOOR.CONTROL_STATE;
+using static STRINGS.ELEMENTS;
+using static STRINGS.UI.NEWBUILDCATEGORIES;
 
 namespace ONIInsulatedSelfSealingAirLock
 {
     [HarmonyPatch(typeof(Door), "SetSimState")]
     public class Door_SetSimState
     {
-        public static void Prefix(Door __instance, bool is_door_open, IList<int> cells)
+        public static void Postfix(Door __instance, bool is_door_open, IList<int> cells)
         {
             if (__instance.PrefabID() != InsulatedSelfSealingAirLockConfig.ID) return;
 
-            for (int i = 0; i < cells.Count; i++)
+            // is_door_open can be incorrect on load, overwrite with control status
+            is_door_open = (__instance.CurrentState == Door.ControlState.Opened);
+
+            foreach (int cell in cells)
             {
-                int cell = cells[i];
-
-                SimMessages.SetInsulation(cell, 0f);
-
-                Door.ControlState controlState = Traverse.Create(__instance).Field("controlState").GetValue<Door.ControlState>();
-                if (controlState != Door.ControlState.Opened)
+                if (is_door_open)
                 {
-                    // GasImpermeable (1), LiquidImpermeable (2), SolidImpermeable (4)
-                    SimMessages.SetCellProperties(cell, 7);
+                    // allow temperature transmission when set to Open
+                    SimMessages.SetInsulation(cell, 1f);
+                
+                    // Allow transmission when set to Open
+                    SimMessages.ClearCellProperties(cell, 7);
                 }
                 else
                 {
+                    // perfect insulator
+                    SimMessages.SetInsulation(cell, 0f);
+                
+                    // GasImpermeable (1), LiquidImpermeable (2), SolidImpermeable (4)
+                    SimMessages.SetCellProperties(cell, 7);
+                }
+            }
+        }
+    }
+
+    [HarmonyPatch(typeof(Door), "OnSpawn")]
+    public class Door_OnSpawn
+    {
+        public static void Postfix(Door __instance)
+        {
+            if (__instance.PrefabID() != InsulatedSelfSealingAirLockConfig.ID) return;
+
+            bool is_door_open = (__instance.CurrentState == Door.ControlState.Opened);
+
+            foreach (int cell in __instance.building.PlacementCells)
+            {
+                if (is_door_open)
+                {
+                    // allow temperature transmission when set to Open
+                    SimMessages.SetInsulation(cell, 1f);
+
                     // Allow transmission when set to Open
                     SimMessages.ClearCellProperties(cell, 7);
+                }
+                else
+                {
+                    // perfect insulator
+                    SimMessages.SetInsulation(cell, 0f);
+
+                    // GasImpermeable (1), LiquidImpermeable (2), SolidImpermeable (4)
+                    SimMessages.SetCellProperties(cell, 7);
                 }
             }
         }
